4437f2b82f009c0f8de43ea269bc7c8f997372a8
Skillyboy <fakoredeabbas@gmail.com>
2024-09-12 02:38:28 +0100

    Fixed URL routing issue and adjusted API testing

--- library_api/settings.py
+++ library_api/settings.py
@@ -72,25 +72,25 @@ WSGI_APPLICATION = "library_api.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
-
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('POSTGRES_DB', 'library'),
-#         'USER': os.getenv('POSTGRES_USER', 'skillyboy'),
-#         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'skilly1234'),
-#         'HOST': os.getenv('DB_HOST', 'localhost'),  # 'db' is the service name from docker-compose.yml
-#         'PORT': os.getenv('DB_PORT', '5432'),
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
 #     }
 # }
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'library'),
+        'USER': os.getenv('POSTGRES_USER', 'skillyboy'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'skilly1234'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),  # 'db' is the service name from docker-compose.yml
+        'PORT': os.getenv('DB_PORT', '5432'),
+    }
+}
 
 
 # Password validation
